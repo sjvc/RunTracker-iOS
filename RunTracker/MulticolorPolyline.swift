ca98bf6b27a1741524e752f1bc7677286fd19cb1
sergio <djpep.dj@gmail.com>
2020-03-04 20:07:41 +0100

    Dibujar recorrido en historial de colores (verde = lento, rojo = rápido)

---
+++ RunTracker/MulticolorPolyline.swift
@@ -0,0 +1,14 @@
+//
+//  MulticolorPolyline.swift
+//  RunTracker
+//
+//  Created by Sergio Viudes Carbonell on 04/03/2020.
+//  Copyright © 2020 Sergio Viudes Carbonell. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MulticolorPolyline: MKPolyline {
+    var color = UIColor.black
+}
--- RunTracker/RunDetailsViewController.swift
+++ RunTracker/RunDetailsViewController.swift
@@ -24,6 +24,15 @@ class RunDetailsViewController: UIViewController {
         loadMap()
     }
     
+    // Carga el mapa
+    private func loadMap() {
+        guard let locations = run!.locations, locations.count > 0, let region = mapRegion() else {
+            print("No se han encontrado ubicaciones almacenadas en el entrenamiento")
+            return
+        }
+        mapView.setRegion(region, animated: true)
+        mapView.addOverlays(polyLine())
+    }
     
     // Devuelve la región del mapa a mostrar según el entrenamiento guardado en "run"
     private func mapRegion() -> MKCoordinateRegion? {
@@ -54,28 +63,85 @@ class RunDetailsViewController: UIViewController {
         return MKCoordinateRegion(center: center, span: span)
     }
     
-    // Devuelve una polilínea con el recorrido
-    private func polyLine() -> MKPolyline {
-        guard let locations = run!.locations else {
-            return MKPolyline()
+    // Devuelve un array de polilíneas, con color, con el recorrido
+    private func polyLine() -> [MulticolorPolyline] {
+        // Preparamos para recoger las coordenadas que forman los segmentos de las polilíneas, y sus velocidades
+        let locations = run!.locations?.array as! [Location]
+        var coordinates: [(CLLocation, CLLocation)] = []
+        var speeds: [Double] = []
+        var minSpeed = Double.greatestFiniteMagnitude
+        var maxSpeed = 0.0
+        
+        // Convertir cada ubicación en CLLocation, y guardar en coordinates los puntos correspondientes a los extremos de los segmentos
+        for (first, second) in zip(locations, locations.dropFirst()) {
+            let start = CLLocation(latitude: first.latitude, longitude: first.longitude)
+            let end = CLLocation(latitude: second.latitude, longitude: second.longitude)
+            // Si la segunda coordenada es un nuevo comienzo (tras una pausa), no tengo en cuenta ese tramo
+            if !second.isNewStart {
+                coordinates.append((start, end))
+            } else {
+                continue
+            }
+            
+            // Calcular la velocidad para el segmento, así como comprobar si es la máxima o la mínima del recorrido
+            let distance = end.distance(from: start)
+            let time = second.date!.timeIntervalSince(first.date! as Date)
+            let speed = time > 0 ? distance / time : 0
+            speeds.append(speed)
+            minSpeed = min(minSpeed, speed)
+            maxSpeed = max(maxSpeed, speed)
         }
         
-        let coords: [CLLocationCoordinate2D] = locations.map { location in
-            let location = location as! Location
-            return CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        // Calcular la velocidad media del recorrido
+        let midSpeed = speeds.reduce(0, +) / Double(speeds.count)
+        
+        // Mediante los pares de coordenadas anteriores, creamos las polilíneas coloreadas
+        var segments: [MulticolorPolyline] = []
+        for ((start, end), speed) in zip(coordinates, speeds) {
+            let coords = [start.coordinate, end.coordinate]
+            let segment = MulticolorPolyline(coordinates: coords, count: 2)
+            segment.color = segmentColor(speed: speed, midSpeed: midSpeed, slowestSpeed: minSpeed, fastestSpeed: maxSpeed)
+            segments.append(segment)
         }
-        return MKPolyline(coordinates: coords, count: coords.count)
+        return segments
     }
     
-    // Carga el mapa
-    private func loadMap() {
-        guard let locations = run!.locations, locations.count > 0, let region = mapRegion() else {
-            print("No se han encontrado ubicaciones almacenadas en el entrenamiento")
-            return
+    // Devuelve un color en función de la velocidad
+    private func segmentColor(speed: Double, midSpeed: Double, slowestSpeed: Double, fastestSpeed: Double) -> UIColor {
+        enum BaseColors {
+            // Color lento (verde)
+            static let slow_red: CGFloat = 76 / 255
+            static let slow_green: CGFloat = 175 / 255
+            static let slow_blue: CGFloat = 80 / 255
+            
+            // Color medio (amarillo)
+            static let mid_red: CGFloat = 1
+            static let mid_green: CGFloat = 235 / 255
+            static let mid_blue: CGFloat = 59 / 255
+            
+            // Color rápido (verde)
+            static let fast_red: CGFloat = 244 / 255
+            static let fast_green: CGFloat = 67 / 255
+            static let fast_blue: CGFloat = 54 / 255
         }
-        mapView.setRegion(region, animated: true)
-        mapView.addOverlay(polyLine())
+        
+        let red, green, blue: CGFloat
+        
+        if speed < midSpeed {
+            let ratio = CGFloat((speed - slowestSpeed) / (midSpeed - slowestSpeed))
+            red = BaseColors.slow_red + ratio * (BaseColors.mid_red - BaseColors.slow_red)
+            green = BaseColors.slow_green + ratio * (BaseColors.mid_green - BaseColors.slow_green)
+            blue = BaseColors.slow_blue + ratio * (BaseColors.mid_blue - BaseColors.slow_blue)
+        } else {
+            let ratio = CGFloat((speed - midSpeed) / (fastestSpeed - midSpeed))
+            red = BaseColors.mid_red + ratio * (BaseColors.fast_red - BaseColors.mid_red)
+            green = BaseColors.mid_green + ratio * (BaseColors.fast_green - BaseColors.mid_green)
+            blue = BaseColors.mid_blue + ratio * (BaseColors.fast_blue - BaseColors.mid_blue)
+        }
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
+    
     
     /*
      // MARK: - Navigation
@@ -93,7 +159,7 @@ extension RunDetailsViewController: MKMapViewDelegate {
     // Se llama a este método cuando se va a pintar sobre el mapa
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = IVBezierPathRenderer(overlay: overlay)
-        renderer.strokeColor = UIColor(red:0.17, green:0.47, blue:0.96, alpha:1.0)
+        renderer.strokeColor = (overlay as! MulticolorPolyline).color
         renderer.lineWidth = 18
         // renderer.borderColor = renderer.strokeColor
         // renderer.borderMultiplier = 1.5
